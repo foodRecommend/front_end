ad8bbec0135ecc48f0db1313317dc2cb00951f98
jeongwon <choimandu31@daum.net>
2022-11-07 22:00:28 +0900

    #dev conflict solved

---
+++ src/pages/Main.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+// import { Button } from '@/elements/Index';
+
+const Main: React.FC = () => {
+  return (
+    <div>
+      Main
+      {/* <Button text="test button" /> */}
+    </div>
+  );
+};
+
+export default Main;
